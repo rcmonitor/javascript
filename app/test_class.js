5ea3bacb3880cce6214773a23b1be83b993bbdeb
rcmonitor <rcmonitor@mail.ru>
2015-09-11 15:04:06 +0300

    replace traceback with stack-trace to satisfy node 4.0.0

--- app/test_class.js
+++ app/test_class.js
@@ -3,9 +3,13 @@
  * on 16.03.2015.
  */
 
-var traceback = require('traceback');
 var os = require('os');
 var eol = os.EOL;
+var util = require('util');
+
+//var traceback = require('traceback');
+//var traceback = require('traceback-safe');
+var stackTrace = require('stack-trace');
 
 function CTestClass(firstParameter){
 	this.publicProperty = 42;
@@ -17,10 +21,23 @@ function CTestClass(firstParameter){
 	this.namedMethod = logInvalid;
 
 	this.insideMethod = function(){
-		var localTrace = traceback()[0];
-		var strReturn = this.invalidEvents.hug + ' from ' + localTrace.name + eol;
 
-		strReturn += 'it is ' + this.outsideEvents.rain + ' in ' + localTrace.name + eol;
+		var arTrace = stackTrace.get();
+
+//		console.log(util.inspect(arTrace[0]));
+
+		var strTestLine = arTrace[0].getFileName() + ': '
+			+ arTrace[0].getLineNumber() + ': '
+			+ arTrace[0].getMethodName() + ' '
+			+ arTrace[0].getFunctionName();
+
+//		console.log(strTestLine);
+
+		var localTrace = arTrace[0].getFunctionName();
+//		var localTrace = 'CTestClass.insideMethod';
+		var strReturn = this.invalidEvents.hug + ' from ' + localTrace + eol;
+
+		strReturn += 'it is ' + this.outsideEvents.rain + ' in ' + localTrace + eol;
 		console.log(strReturn);
 
 		return strReturn;
@@ -44,18 +61,28 @@ console.log(CTestClass.validEvents.fire);
 //console.log(CTestClass.invalidEvents.hug);
 
 CTestClass.prototype.outsideMethod = function(){
-	var localTrace = traceback()[0];
-	var strReturn = this.invalidEvents.hug + ' from ' + localTrace.name + eol;
-	strReturn += this.outsideEvents.rain + ' in ' + localTrace.name + eol;
-	console.log();
+//	var localTrace = traceback()[0];
+
+	var arTrace = stackTrace.get();
+	var strFunction = arTrace[0].getFunctionName();
+
+	var strReturn = this.invalidEvents.hug + ' from ' + strFunction + eol;
+	strReturn += this.outsideEvents.rain + ' in ' + strFunction + eol;
+//	console.log();
 
 	return strReturn;
 };
 
 function logInvalid(){
-	var localTrace = traceback()[0];
-	var strReturn = this.invalidEvents.hug + ' from ' + localTrace.name + eol;
-	strReturn += this.outsideEvents.rain + ' in ' + localTrace.name + eol;
+
+	var arTrace = stackTrace.get();
+
+//	var localTrace = traceback()[0];
+
+	var strFunctionName = arTrace[0].getFunctionName();
+
+	var strReturn = this.invalidEvents.hug + ' from ' + strFunctionName + eol;
+	strReturn += this.outsideEvents.rain + ' in ' + strFunctionName + eol;
 	console.log(strReturn);
 
 	return strReturn;
